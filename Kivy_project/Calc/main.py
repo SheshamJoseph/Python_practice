d455174a5906917431a2a59c0cd82d8e5f109c14
SheshamJoseph <sheshamabraham@gmail.com>
2023-11-12 14:49:31 +0100

    added functonality to calculator buttons

--- Kivy_project/Calc/main.py
+++ Kivy_project/Calc/main.py
@@ -24,23 +24,46 @@ class MyLayout(Widget):
             prior = self.ids.calc_input.text
         self.ids.calc_input.text = "".join([prior, f"{number}"])
     
-    def add(self):
+    def math_sign(self, sign):
         prior = self.ids.calc_input.text
-        self.ids.calc_input.text = f"{prior}+"
-
-    def subtract(self):
-        prior = self.ids.calc_input.text
-        self.ids.calc_input.text = f"{prior}-"
-
-    def divide(self):
-        prior = self.ids.calc_input.text
-        self.ids.calc_input.text = f"{prior}/"
-
-    def multiply(self):
-        prior = self.ids.calc_input.text
-        self.ids.calc_input.text = f"{prior}*"
+        self.ids.calc_input.text = f"{prior}{sign}"
         
+    def equals(self):
+        prior = self.ids.calc_input.text
+        """if "+" in prior:
+            num_list = prior.split("+")
+            answer = 0.0
+            for number in num_list:
+                answer += float(number)
+            
+            self.ids.calc_input.text = str(answer)"""
+        try:
+            answer = eval(prior)
+        except:
+            self.ids.calc_input.text = "Math Error"
+        else:
+            self.ids.calc_input.text = f"{answer}"
 
+    def dot(self):
+        prior = self.ids.calc_input.text
+        num_list = prior.split("+")
+        if "+" in prior and "." not in num_list[-1]:
+             self.ids.calc_input.text = f"{prior}."
+        elif "." in prior:
+            pass
+        else:
+            self.ids.calc_input.text = f"{prior}."
+
+    def delete(self):
+        prior = self.ids.calc_input.text
+        self.ids.calc_input.text = f"{prior[:-1]}"
+    
+    def sign_inverse(self):
+        prior = self.ids.calc_input.text
+        if prior[0] == "-":
+            self.ids.calc_input.text = f"{prior.replace('-', '')}"
+        else:
+            self.ids.calc_input.text = f"-{prior}"
 class CalculatorApp(App):
     def build(self):
         return MyLayout()
